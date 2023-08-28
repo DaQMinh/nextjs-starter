cd8568eca2149fdcef10206a03f50798a669a79c
DaoMinhWysi <114815151+DaQMinh@users.noreply.github.com>
2023-08-28 13:27:52 +0700

    add shadcn/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,31 +1,19 @@
 // These styles apply to every route in the application
 import "@/styles/globals.css";
-import { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Toaster } from "react-hot-toast";
-import { Suspense } from "react";
 import AuthContext from "./context/AuthContext";
+import { ThemeProvider } from "@/components/theme/theme-provider";
+import { Metadata } from "next";
 
 const inter = Inter({
   variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const title = "Next.js Prisma Postgres Auth Starter";
-const description =
-  "This is a Next.js starter kit that uses Next-Auth for simple email + password login and a Postgres database to persist the data.";
-
 export const metadata: Metadata = {
-  title,
-  description,
-  twitter: {
-    card: "summary_large_image",
-    title,
-    description,
-  },
-  metadataBase: new URL("https://nextjs-postgres-auth.vercel.app"),
-  themeColor: "#FFF",
-};
+  title: 'Next Cofession',
+  description: 'Automation the process of a Cofession\' Mod',
+}
 
 export default async function RootLayout({
   children,
@@ -34,13 +22,13 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en">
-      
       <body className={inter.variable}>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <AuthContext>
         {children}
         </AuthContext>
-      </body>
-      
+        </ThemeProvider>
+      </body>      
     </html>
   );
 }
